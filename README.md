85ee1838825ef959b90f1ae8943e3d0eed6f83ae
kajasharif31 <kajasharif366@gmail.com>
2024-02-26 00:37:23 +0530

    feat: MongoDB Connection established Successfully

---
+++ README.md
@@ -0,0 +1,8 @@
+# Express MongoDB
+
+- [x] setup express
+- [x] connect MongoDB
+
+#### Reference
+
+[FreeCode Camp NodeJS, MongoDB YT video](https://www.youtube.com/watch?v=_7UQPve99r4)
--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import express from "express";
+import mongoose from "mongoose";
+
+import dotenv from "dotenv"; // for using env variables
+dotenv.config();
 
 const server = express();
 const SERVER_PORT = 2024;
@@ -7,7 +11,23 @@ server.listen(SERVER_PORT, () => {
   console.log(`Server is runnig in localhost:${SERVER_PORT}`);
 });
 
-server.get("/", (req, res) => {
-  res.json({ status: 200, message: "express setup to home route (/)" });
-  res.end();
-});
+// checking the server is connected to DB then we perform our logics
+mongoose
+  .connect(process.env.MONGO_DB_CONNECTION_URL)
+  .then(() => {
+    console.log("sucessfully Connected to MongoDB");
+
+    // GET REQ
+    server.get("/", (req, res) => {
+      res.json({ status: 200, message: "express setup to home route (/)" });
+      res.end();
+    });
+
+    // POST
+    // PUT
+    // DELETE
+    // etc....
+  })
+  .catch(() => {
+    console.log("Failed Connect MongoDB");
+  });
