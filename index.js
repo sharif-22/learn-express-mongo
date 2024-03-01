9681ba8409e03d08ca8d25aa628f3c792b89f012
kajasharif31 <kajasharif366@gmail.com>
2024-03-01 11:34:51 +0530

    proper setup to listen server

--- index.js
+++ index.js
@@ -7,26 +7,26 @@ dotenv.config();
 const server = express();
 const SERVER_PORT = 2024;
 
-server.listen(SERVER_PORT, () => {
-  console.log(`Server is runnig in localhost:${SERVER_PORT}`);
+// GET REQ
+server.get("/", (req, res) => {
+  res.json({ status: 200, message: "express setup to home route (/)" });
+  res.end();
 });
 
+// POST
+// PUT
+// DELETE
+// etc....
+
 // checking the server is connected to DB then we perform our logics
+// or else listen the port after DB connected
 mongoose
   .connect(process.env.MONGO_DB_CONNECTION_URL)
   .then(() => {
     console.log("sucessfully Connected to MongoDB");
-
-    // GET REQ
-    server.get("/", (req, res) => {
-      res.json({ status: 200, message: "express setup to home route (/)" });
-      res.end();
+    server.listen(SERVER_PORT, () => {
+      console.log(`Server is running in localhost:${SERVER_PORT}`);
     });
-
-    // POST
-    // PUT
-    // DELETE
-    // etc....
   })
   .catch(() => {
     console.log("Failed Connect MongoDB");
